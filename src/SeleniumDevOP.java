29544a90ad09241f834b2c24a1ec7b5b154d7935
khadershaik <khadershaik@dsn4.skype>
2018-02-21 01:34:27 -0800

    Initial Commit for Selenium Changes on Test Design

---
+++ src/SeleniumDevOP.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author khadershaik
+ *
+ */
+public class SeleniumDevOP {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+System.out.println("Selenium DevOP Strategy!");
+System.out.println("Checkin TO GIT Repo!");
+	
+	
+	}
+
+}
